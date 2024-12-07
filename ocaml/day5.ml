40e8dfa12bdac568660f744069e9005d0b82f83a
Federico <dalpio23@gmail.com>
2024-12-08 00:33:09 +0100

    day 5 part 2

--- ocaml/day5.ml
+++ ocaml/day5.ml
@@ -7,50 +7,30 @@ let pa = P.(many (both (U.int <* char '|') U.int))
 
 let pl = P.(each_line (sep ~by:(char ',') U.int))
 
-let is_transitive (l : (int * int) list) =
-  let open L in
-  let* x, y = l and* y', z = l in
-  return ((not (Int.equal y y')) || List.mem (x, z) l)
+let compare_assoc order x y : int =
+  if L.mem (x, y) order then
+    -1
+  else if L.mem (y, x) order then
+    1
+  else
+    0
 
-let rec check ord = function
-  | [] | [ _ ] -> true
-  | a :: b :: xs -> List.mem (a, b) ord && check ord (b :: xs)
-(* no *)
-
-let check order pages =
-  let open L in
-  let* x, y = order in
-  let* ps = pages in
-  return
-  @@
-  let open O in
-  let* ix = L.find_index (Int.equal x) ps in
-  let* iy = L.find_index (Int.equal y) ps in
-  if_ (fun _ -> ix <= iy) ps
-(* no *)
-
-let check order pages =
-  let open L in
-  filter_map
-    (fun ps ->
-      if
-        for_all
-          (fun (x, y) ->
-            let open O in
-            for_all (( = ) true)
-            @@
-            let* ix = L.find_index (Int.equal x) ps in
-            let* iy = L.find_index (Int.equal y) ps in
-            pure (ix <= iy))
-          order
-      then
-        nth_opt ps (length ps / 2)
-      else
-        None)
-    pages
-
-let solve input_order input_pages =
+let solve1 input_order input_pages =
   let open R in
   let* order = P.parse_file pa input_order in
   let* pages = P.parse_file pl input_pages in
-  check order pages |> L.fold_left ( + ) 0 |> return
+  L.(filter (is_sorted ~cmp:(compare_assoc order))) pages
+  |> L.(map (fun ps -> nth_opt ps (length ps / 2)))
+  |> L.keep_some |> L.fold_left ( + ) 0 |> return
+
+let solve2 input_order input_pages =
+  let open R in
+  let* order = P.parse_file pa input_order in
+  let* pages = P.parse_file pl input_pages in
+  L.(filter CCFun.(not % is_sorted ~cmp:(compare_assoc order))) pages
+  |> L.(map (sort (compare_assoc order)))
+  |> L.(map (fun ps -> nth_opt ps (length ps / 2)))
+  |> L.keep_some |> L.fold_left ( + ) 0 |> return
+
+let assoc = P.parse_file pa "5a.txt" |> Result.get_ok
+let pages = P.parse_file pl "5b.txt" |> Result.get_ok
