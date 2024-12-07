5ffa6a6694b0647e0db88ac042eea4a250459b54
Federico <dalpio23@gmail.com>
2024-12-07 13:36:09 +0100

    day 4 part 2

--- ocaml/day4.ml
+++ ocaml/day4.ml
@@ -9,6 +9,8 @@ type dir = { dx : dir_x; dy : dir_y }
 
 type coord = { x : int; y : int }
 
+let string_of_coord p = Printf.sprintf "{%d,%d}" p.x p.y
+
 type solution = dir * coord
 
 (* If centered on a 'X', look for 'M' on adjacent, fix the direction and look for 'A' and 'S' in that direction *)
@@ -47,31 +49,49 @@ let lookup (p : coord) (board : 'a list list) : 'a option =
   let* xs = L.nth_opt board p.y in
   L.nth_opt xs p.x
 
-let match_xmas (d : dir) (p : coord) (board : char list list) =
+let match_goal (goal : string) (d : dir) (p : coord) (board : char list list) =
   let d = coord_of_dir d in
+  let open L in
+  let path = combine (CCString.to_list goal) (0 --^ CCString.length goal) in
+  let open O in
+  let* _ =
+    assume
+    @@ L.for_all
+         (fun (c, step) ->
+           match
+             lookup (mk_coord (p.x + (d.x * step)) (p.y + (d.y * step))) board
+           with
+           | Some x when CCChar.equal x c -> true
+           | _ -> false)
+         path
+  in
+  return (mk_solution d p)
+
+let match_xmas = match_goal "XMAS"
+
+let match_x_mas (p : coord) (board : char list list) =
   let open O in
   let open CCChar in
-  let* x = lookup (mk_coord (p.x + (d.x * 0)) (p.y + (d.y * 0))) board in
-  print_char x;
-  let* _ = assume (x = 'X') in
-  let* x = lookup (mk_coord (p.x + (d.x * 1)) (p.y + (d.y * 1))) board in
-  print_char x;
-  let* _ = assume (x = 'M') in
-  let* x = lookup (mk_coord (p.x + (d.x * 2)) (p.y + (d.y * 2))) board in
-  print_char x;
-  let* _ = assume (x = 'A') in
-  let* x = lookup (mk_coord (p.x + (d.x * 3)) (p.y + (d.y * 3))) board in
-  print_char x;
-  let* _ = assume (x = 'S') in
-  print_newline ();
-  return (mk_solution d p)
+  let* a = lookup (mk_coord p.x p.y) board in
+  let* _ = assume (a = 'A') in
+  let* tl = lookup (mk_coord (p.x - 1) (p.y - 1)) board in
+  let* tr = lookup (mk_coord (p.x + 1) (p.y - 1)) board in
+  let* bl = lookup (mk_coord (p.x - 1) (p.y + 1)) board in
+  let* br = lookup (mk_coord (p.x + 1) (p.y + 1)) board in
+  let* _ =
+    assume
+      (tl <> br && tr <> bl
+      && L.for_all (fun x -> x = 'M' || x = 'S') [ tl; tr; bl; br ])
+  in
+  Printf.printf "%c.%c\n.%c.\n%c.%c\n%s\n\n" tl tr a bl br (string_of_coord p);
+  return p
 
 let p = P.(each_line (many any_char))
 
 let parse_board input =
   P.parse_file p input |> Result.get_ok |> L.filter (fun l -> l <> [])
 
-let search (b : char list list) =
+let search1 (b : char list list) =
   let open L in
   foldi
     (fun acc1 j xs ->
@@ -81,6 +101,14 @@ let search (b : char list list) =
         acc1 xs)
     [] b
 
-;;
+let search2 (b : char list list) =
+  let open L in
+  foldi
+    (fun acc1 j xs ->
+      foldi (fun acc2 i _ -> match_x_mas (mk_coord i j) b :: acc2) acc1 xs)
+    [] b
+  |> keep_some
 
-parse_board "day4.input" |> search |> L.length;;
+let b = parse_board "day4.input";;
+b |> search1 |> L.length;;
+b |> search2 |> L.length (* 1st: 17160; 2nd: 2145 *)
