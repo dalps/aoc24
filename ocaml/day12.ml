dae1ab5183568b562d81fd2ceb9e5bfe62f6ae54
Federico <dalpio23@gmail.com>
2024-12-14 01:08:11 +0100

    day 12 part 2

--- ocaml/day12.ml
+++ ocaml/day12.ml
@@ -3,8 +3,15 @@ open Utils
 let p = P.(each_line (many any_char))
 
 module Plot = struct
-  type t = { name : char; p : Coord.t; mutable n_neighbors : int }
-  let make name p = { name; p; n_neighbors = 0 }
+  type t = {
+    name : char;
+    p : Coord.t;
+    mutable n_neighbors : int;
+    boundary : bool array;
+  }
+  [@@deriving show]
+
+  let make name p = { name; p; n_neighbors = 0; boundary = A.make 4 false }
 
   let hash p = Coord.hash p.p
   let compare p1 p2 = Coord.compare p1.p p2.p
@@ -16,24 +23,37 @@ module SCC = Graph.Components.Make (PG)
 
 let mk_plots input =
   P.parse_file p input |> R.get_exn |> LM.remove_empty_rows
-  |> LM.mapij (fun y x c -> Plot.make c Coord.(make x y))
+  |> LM.mapij (fun y x c -> Plot.make c (Coord.make x y))
+
+let set_wrap (a : bool array) (i : int) = a.(i mod A.length a) <- true
+
+let show_compass = function
+  | 0 -> "north"
+  | 1 -> "east"
+  | 2 -> "south"
+  | _ -> "west"
 
 let build_graph plots =
   let g = PG.create () in
   LM.iterij
     (fun _ _ (p : Plot.t) ->
-      let open L in
       PG.add_vertex g p;
-      L.iter
-        (fun d ->
+      L.iteri
+        (fun i d ->
+          (* 0 is north, clockwise *)
           let p' = Coord.(p.p + d) in
           match LM.get plots p'.x p'.y with
-          | None -> ()
-          | Some (n : Plot.t) ->
-              if CCChar.equal p.name n.name then (
-                p.n_neighbors <- p.n_neighbors + 1;
-                PG.add_edge g p n))
-        Coord.compass)
+          | Some (n : Plot.t) when CCChar.equal p.name n.name ->
+              p.n_neighbors <- p.n_neighbors + 1;
+              PG.add_edge g p n
+          | Some other ->
+              Format.printf
+                "Facing %s, I found that @.%c at %a borders with @.%c at %a\n"
+                (show_compass i) p.name Coord.pp p.p other.name Coord.pp other.p;
+              set_wrap p.boundary i;
+              set_wrap other.boundary (i + 2)
+          | None -> set_wrap p.boundary i)
+        Coord.compass0)
     plots;
   g
 
@@ -52,4 +72,58 @@ let solve1 input =
       in
       pr "perimeter: %d, area: %d\n" peri area;
       acc + (peri * area))
+    0 scc
+
+let rec get_side_aux x y ((side, sides) as accu) (plots : Plot.t list) =
+  match plots with
+  | [] -> accu
+  | [ p ] -> ([], (p :: side) :: sides)
+  | p1 :: p2 :: ps ->
+      let accu' =
+        if x p1.p = x p2.p && abs (y p1.p - y p2.p) = 1 then
+          (* continue current side *)
+          (p1 :: side, sides)
+        else (* start a new side *)
+          ([], (p1 :: side) :: sides)
+      in
+      get_side_aux x y accu' (p2 :: ps)
+
+let get_sides x y (plots : Plot.t list) : Plot.t list list =
+  get_side_aux x y ([], []) plots |> snd
+
+let order x y (p1 : Plot.t) (p2 : Plot.t) =
+  let p1, p2 = (p1.p, p2.p) in
+  let c0 = Int.compare (x p1) (x p2) in
+  if c0 = 0 then Int.compare (y p1) (y p2) else c0
+
+let solve2 input =
+  let open L in
+  let plots = mk_plots input in
+  let g = build_graph plots in
+  let scc = SCC.scc_list g in
+  fold_left
+    (fun acc region ->
+      (* Format.printf "region = @[%a@]@." (pp Plot.pp) region; *)
+      (* for every kind of face (north, east...) *)
+      let area = length region in
+      let peri = ref 0 in
+      for i = 0 to 3 do
+        let boundary = filter (fun (p : Plot.t) -> p.boundary.(i)) region in
+        let x', y' = Coord.(if i mod 2 = 0 then (y, x) else (x, y)) in
+        let order (p1 : Plot.t) (p2 : Plot.t) =
+          let p1, p2 = (p1.p, p2.p) in
+          let c0 = Int.compare (x' p1) (x' p2) in
+          if c0 = 0 then Int.compare (y' p1) (y' p2) else c0
+        in
+        let sorted = sort order boundary in
+        Format.printf "b = @[%a@]@." (pp Plot.pp) sorted;
+        let sides = sorted |> get_sides x' y' >|= L.length in
+        let (sample : Plot.t) = hd region in
+        Format.printf
+          "For region %c at %a, facing %s, I found the following sides @[%a@]@."
+          sample.name Coord.pp sample.p (show_compass i) (pp CCInt.pp) sides;
+
+        peri := !peri + length sides
+      done;
+      acc + (area * !peri))
     0 scc
--- ocaml/utils.ml
+++ ocaml/utils.ml
@@ -1,4 +1,5 @@
 module L = CCList
+module LA = L.Assoc
 module O = CCOption
 module P = CCParse
 module R = CCResult
@@ -9,14 +10,195 @@ module Pr = Printf
 let pr = Pr.printf
 let spr = Pr.sprintf
 
+open F
+
+let get_input (p : 'a P.t) (f : string) = P.parse_file p f |> R.get_or_failwith
 
 let read_file (filename : string) : string =
   let fd = open_in filename in
   let s = really_input_string fd (in_channel_length fd) in
-  close_in fd; s
+  close_in fd;
+  s
 
 let is_transitive (l : (int * int) list) =
   let open L in
-  fold_left ( && ) true @@
+  fold_left ( && ) true
+  @@
   let* x, y = l and* y', z = l in
   return ((not (Int.equal y y')) || List.mem (x, z) l)
+
+let filter_map_i f = snd % L.fold_filter_map_i (fun _ i x -> ((), f i x)) ()
+
+(* filter elements of a list by a function based on the index of elements as well *)
+let filter_mapi f l =
+  let[@tail_mod_cons] rec aux i = function
+    | [] -> []
+    | x :: xs -> (
+        match f i x with
+        | None -> aux (i + 1) xs
+        | Some x -> x :: aux (i + 1) xs)
+  in
+  aux 0 l
+
+let assume (b : bool) : unit option =
+  if b then
+    Some ()
+  else
+    None
+
+let rec fold_left_pair (f : 'acc -> 'a -> 'a -> 'acc) (accu : 'acc) = function
+  | [] | [ _ ] -> accu
+  | x0 :: x1 :: xs -> fold_left_pair f (f accu x0 x1) (x1 :: xs)
+
+let fold_left_w2 = fold_left_pair
+
+let rec fold_left_w3 (f : 'acc -> 'a -> 'a -> 'a -> 'acc) (accu : 'acc) =
+  function
+  | [] | [ _ ] | [ _; _ ] -> accu
+  | x0 :: x1 :: x2 :: xs -> fold_left_w3 f (f accu x0 x1 x2) (x1 :: x2 :: xs)
+
+module Coord = struct
+  type t = { x : int; y : int }
+  [@@deriving show]
+
+  let make x y : t = { x; y }
+  let v x y : t = { x; y }
+  let x (p : t) : int = p.x
+  let y (p : t) : int = p.y
+
+  let to_string (c : t) = spr "{%d,%d}" c.x c.y
+  let pr (c : t) = pr "%s" (to_string c)
+
+  let compare p1 p2 = CCOrd.(pair int int) (p1.x, p1.y) (p2.x, p2.y)
+  let equal p1 p2 = Int.(equal p1.x p2.x && equal p1.y p2.y)
+  let hash p = CCHash.(pair int int) (p.x, p.y)
+
+  let ( *. ) a p = v (a * p.x) (a * p.y)
+  let ( * ) p1 p2 = (p1.x * p2.x) + (p1.y * p2.y)
+  let ( + ) p1 p2 = v (p1.x + p2.x) (p1.y + p2.y)
+  let ( - ) p1 p2 = v (p1.x - p2.x) (p1.y - p2.y)
+
+  let north = make 0 (-1)
+  let east = make 1 0
+  let south = -1 *. north
+  let west = -1 *. east
+
+  let compass0 = [ north; east; south; west ]
+  let compass1 = [ (`N, north); (`E, east); (`S, south); (`W, west) ]
+  let compass2 = [ `N north; `E east; `S south; `W west ]
+end
+
+module ListMatrix = struct
+  type 'a t = 'a list list
+
+  let get (board : 'a t) i j : 'a option =
+    let open O in
+    let* _ = assume (i >= 0 && j >= 0) in
+    let* xs = L.nth_opt board j in
+    L.nth_opt xs i
+
+  let remove_empty_rows (t : 'a list list) : 'a list list =
+    L.filter (not % L.is_empty) t
+
+  let foldij f (acc : 'acc) m =
+    L.foldi
+      (fun acc1 i xs -> L.foldi (fun acc2 j x -> f acc2 i j x) acc1 xs)
+      acc m
+
+  let iterij p = foldij (fun _ i j x -> p i j x) ()
+
+  let filter_mapij (f : int -> int -> 'a -> 'b option) =
+    foldij (fun acc i j x -> L.cons_maybe (f i j x) acc) []
+
+  let filterij (f : int -> int -> 'a -> bool) =
+    filter_mapij (fun i j x -> if f i j x then Some x else None) []
+
+  let mapij (f : int -> int -> 'a -> 'b) = L.mapi (L.mapi % f)
+
+  let map f (m : 'a list) =
+    let open L in
+    let+ m = m in
+    f <$> m
+
+  let size : 'a t -> [ `Square of int | `Rect of int * int | `Hetero ] =
+    function
+    | [] -> `Square 0
+    | x :: xs as l ->
+        let nrow = L.length l in
+        let ncol = L.length x in
+        let others = L.map L.length xs in
+        if L.for_all (( = ) ncol) others then
+          if nrow = ncol then
+            `Square nrow
+          else
+            `Rect (nrow, ncol)
+        else
+          `Hetero
+end
+
+module ArrayMatrix = struct
+  type 'a t = 'a array array
+
+  (* Ideas: array to list functions *)
+  let get (board : 'a t) i j : 'a option =
+    let open O in
+    let* xs = A.get_safe board j in
+    A.get_safe xs i
+
+  let set (board : 'a t) i j x : unit =
+    if O.is_some (get board i j) then
+      board.(j).(i) <- x
+    else
+      ()
+
+  let find (p : 'a -> bool) (board : 'a t) =
+    let open A in
+    find_mapi
+      (fun j ts -> find_mapi (fun i x -> if p x then Some (i, j) else None) ts)
+      board
+
+  let print ~(print_a : 'a -> unit) = A.iter (print_newline % A.iter print_a)
+
+  let of_list (t : 'a list list) : 'a t = t |> A.of_list %> A.map A.of_list
+end
+
+module LM = ListMatrix
+module AM = ArrayMatrix
+
+module type BASE_FOLDING = sig
+  val fold : ('acc -> 'a -> 'acc) -> 'acc -> 'a list -> 'acc
+  val foldi : ('acc -> int -> 'a -> 'acc) -> 'acc -> 'a list -> 'acc
+end
+
+module type MORE_FOLDING = sig
+  val fold2 : ('acc -> 'a -> 'b -> 'acc) -> 'acc -> 'a list -> 'b list -> 'acc
+  val foldi2 :
+    ('acc -> int -> 'a -> 'b -> 'acc) -> 'acc -> 'a list -> 'b list -> 'acc
+end
+
+module type FOLDING = sig
+  include BASE_FOLDING
+  include MORE_FOLDING
+end
+
+module Make_folding (F : BASE_FOLDING) : FOLDING = struct
+  include F
+
+  let fold2 f init a b =
+    F.fold (fun acc (a, b) -> f acc a b) init (L.combine_shortest a b)
+
+  let foldi2 f init a b =
+    F.foldi (fun acc i (a, b) -> f acc i a b) init (L.combine_shortest a b)
+end
+
+let digits_of_int n =
+  let rec go acc n = if n > 0 then go ((n mod 10) :: acc) (n / 10) else acc in
+  if n = 0 then [ 0 ] else go [] n
+
+let int_of_digits ds =
+  let open CCInt in
+  let rec go mul = function
+    | [] -> 0
+    | d :: ds -> (d * (10 ** mul)) + go (mul - 1) ds
+  in
+  go (L.length ds - 1) ds
