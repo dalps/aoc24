96e7d912b98e67c2f45761bc245352a1ad535753
Federico <dalpio23@gmail.com>
2024-12-20 01:14:05 +0100

    try harder

--- ocaml/day15.ml
+++ ocaml/day15.ml
@@ -29,11 +29,15 @@ let p_board =
 
 let get_data input_a input_b =
   let open L in
-  let board = P.parse_file_exn p_board (data input_a) |> AM.of_list in
+  let board =
+    P.parse_file_exn p_board (data input_a)
+    |> LM.remove_empty_rows |> AM.of_list
+  in
   let program = P.parse_file_exn p_program (data input_b) |> fst |> rev in
   (board, program)
 
 let b, p = get_data "15a.txt" "15b.txt"
+let b', p' = get_data "day15.input" "day15.moves"
 
 let get_boxes (board : space AM.t) =
   let open AM in
@@ -80,13 +84,23 @@ let rec push_box board dir box_pos =
 let double_board =
   A.map (A.flat_map (function P -> [| P; E |] | x -> [| x; x |]))
 
-let print_board = AM.print ~print_a:F.(print_string % string_of_space)
+let print_board board =
+  let open A in
+  print_string "  ";
+  iter print_int (0 --^ 10);
+  iter print_int (0 --^ 10);
+  print_newline ();
+  iteri
+    (fun i row ->
+      Printf.printf "%d %s\n" i
+      @@ fold_left (fun acc col -> acc ^ string_of_space col) "" row)
+    board
 
 let%expect_test "test_double_board" =
   let b2 = double_board b in
   print_board b2
 
-let move_box board (box : box) dir =
+let move_box boxes board (box : box) dir =
   let open AM in
   let open P2 in
   let p' = box.p + dir in
@@ -102,6 +116,9 @@ let move_box board (box : box) dir =
     board.-(p') <- B box.id;
     board.-(p' + east) <- B box.id;
     board.-(box.p - dir) <- E);
+  pr "Setting box %d's position: from %s to %s\n" box.id (P2.to_string box.p)
+    (P2.to_string p');
+  Hashtbl.replace boxes box.id { box with p = p' };
   box.p <- p'
 
 let move_bot board (bot : robot) dir =
@@ -112,7 +129,9 @@ let move_bot board (bot : robot) dir =
   board.-(p') <- P;
   bot.p <- p'
 
-let check_adjacent (board : space AM.t) box dir =
+let string_of_o ~a:pp_a = function None -> "none" | Some a -> pp_a a
+
+let check_adjacent boxes (board : space AM.t) box dir =
   let open P2 in
   let open AM in
   let open R in
@@ -125,34 +144,44 @@ let check_adjacent (board : space AM.t) box dir =
     else
       [ box + (2 *. dir) ]
   in
-  L.map (fun v -> (v, board.-(v))) vs |> function
-  | (_, None) :: _ | (_, Some W) :: _ -> fail_printf "Touching wall"
-  | [ (p, Some (B id)) ] -> pure [ { id; p } ]
-  | (v1, Some (B b1)) :: (v2, Some (B b2)) :: _ ->
-      if b1 = b2 then
-        (* Returning new references!!! *)
-        pure [ { id = b1; p = v1 } ]
-      else
-        pure [ { id = b1; p = v1 + west }; { id = b2; p = v2 } ]
-  | _ ->
-      pr "Seems to be empty...\n";
-      pure []
+  L.map
+    (fun v ->
+      let b = board.-(v) in
+      pr "Checking point %s: %s\n" (P2.to_string v)
+        (string_of_o ~a:string_of_space b);
+      b)
+    vs
+  |> L.keep_some
+  |> fun spaces ->
+  if L.mem W spaces then
+    fail_printf "Touching wall"
+  else
+    match spaces with
+    | [ B id ] | [ E; B id ] | [ B id; E ] -> pure [ Hashtbl.find boxes id ]
+    | [ B b1; B b2 ] ->
+        if b1 = b2 then (* Returning new references!!! *)
+          pure [ Hashtbl.find boxes b1 ]
+        else
+          pure [ Hashtbl.find boxes b1; Hashtbl.find boxes b2 ]
+    | _ ->
+        pr "Seems to be empty...\n";
+        pure []
 
-let rec push_boxes q board dir box =
+let rec push_boxes boxes q board dir box =
   let open P2 in
   let open R in
-  let* nexts = check_adjacent board box.p compass.(dir) in
+  let* nexts = check_adjacent boxes board box.p compass.(dir) in
   let* _ =
     Utils.pr "Found %d adjacent boxes to box %d\n" (L.length nexts) box.id;
     L.fold_left
       (fun acc box' ->
         let* _ = acc in
         pr "Good to go\n";
-        push_boxes q board dir box')
+        push_boxes boxes q board dir box')
       (Ok q) nexts
   in
   pr "Scheduled move for box %d\n" box.id;
-  Queue.add (fun () -> move_box board box compass.(dir)) q;
+  Queue.add (fun () -> move_box boxes board box compass.(dir)) q;
   pure q
 
 let exec1 board (bot : robot) boxes =
@@ -170,7 +199,7 @@ let exec1 board (bot : robot) boxes =
         ignore
         @@
         let q = Queue.create () in
-        let* q = push_boxes q board dir box in
+        let* q = push_boxes boxes q board dir box in
         pr "Moving boxes...\n";
         Queue.iter (fun move -> move ()) q;
         pr "Boxes moved.\n";
@@ -197,27 +226,45 @@ let exec board program =
     (fun i ->
       exec1 board bot boxes i;
       print_board board)
-    program
+    program;
+  boxes
 
+(*
+1507817 (* too low *)
+759079
+1543997 (* too high *)
+*)
 let%expect_test "test_exec" =
   let open L in
-  let p = P.parse_file_exn p_program (data "15b.txt") |> fst |> rev in
-  let b2 = double_board b in
-  exec b2 (take 200 p)
-
-let solve1 input_a input_b =
-  let b, p = get_data input_a input_b in
-  exec b p;
-  let boxes =
-    AM.filter_mapij (fun x y -> function B _ -> Some (x, y) | _ -> None) b
+  let b, p = if true then b, p else b', p' in
+  let b = double_board b in
+  let boxes0 = get_boxes b |> CCHashtbl.values_list |> length in
+  let boxes = exec b p in
+  print_endline "done";
+  print_board b;
+  let boxes = boxes |> CCHashtbl.values_list in
+  let size = P2.make (A.length b.(0)) (A.length b) in
+  pr "Size: %d, %d\n" size.x size.y;
+  pr "There were %d boxes\n" boxes0;
+  pr "There are %d boxes\n" (length boxes);
+  let answer =
+    L.fold_left (fun acc box -> (100 * box.p.y) + box.p.x + acc) 0 boxes
   in
-  L.fold_left (fun acc (x, y) -> (100 * y) + x + acc) 0 boxes
+  print_int answer
+
+(* let solve1 input_a input_b =
+   let b, p = get_data input_a input_b in
+   exec b p;
+   let boxes =
+     AM.filter_mapij (fun x y -> function B _ -> Some (x, y) | _ -> None) b
+   in
+   L.fold_left (fun acc (x, y) -> (100 * y) + x + acc) 0 boxes *)
 
 (* let%expect_test "solve1" =
-     let r = solve1 "15a.txt" "15b.txt" in
-     print_int r;
-     [%expect {| 10092 |}]
+   let r = solve1 "15a.txt" "15b.txt" in
+   print_int r;
+   [%expect {| 10092 |}] *)
 
-   let%expect_test "solve1'" =
-     let r = solve1 "day15.input" "day15.moves" in
-     print_int r *)
+(* let%expect_test "solve1'" =
+   let r = solve1 "day15.input" "day15.moves" in
+   print_int r *)
